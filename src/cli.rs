5b5d48e94e629d3075e76a93902e6616ff29be76
stringhandler <mikethetike@tari.com>
2023-06-07 10:15:53 +0200

    fix: various fixes (#2)
    * fix: propose when new qc is available
    
    * fix: new transactions

--- src/cli.rs
+++ src/cli.rs
@@ -4,9 +4,9 @@ use clap::Parser;
 pub struct Cli {
     #[clap(short, long, default_value = "20")]
     pub num_vns: usize,
-    #[clap(long, default_value = "100ms")]
+    #[clap(long, default_value = "50ms")]
     pub min_latency: humantime::Duration,
-    #[clap(long, default_value = "100ms")]
+    #[clap(long, default_value = "50ms")]
     pub max_latency: humantime::Duration,
     #[clap(long, default_value = "10")]
     pub max_block_size: usize,
@@ -17,7 +17,7 @@ pub struct Cli {
     #[clap(long, default_value = "5000ms")]
     pub delta: humantime::Duration,
 
-    #[clap(long, default_value = "200")]
+    #[clap(long, default_value = "40")]
     pub num_steps: usize,
     #[clap(long, default_value = "100ms")]
     pub time_per_step: humantime::Duration,
@@ -28,12 +28,12 @@ pub struct Cli {
     #[clap(long, default_value = "5")]
     pub max_tx_per_step_per_block: usize,
 
-    #[clap(long, default_value = "50")]
+    #[clap(long, default_value = "500")]
     pub num_transactions: usize,
 
-    #[clap(long, default_value = "5")]
+    #[clap(long, default_value = "10")]
     pub probability_2_shards: u32,
-    #[clap(long, default_value = "0")]
+    #[clap(long, default_value = "5")]
     pub probability_3_shards: u32,
     #[clap(long, default_value = "0")]
     pub probability_4_shards: u32,
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,31 +1,44 @@
 use std::cmp::Ordering;
+use std::sync::Arc;
 
 #[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Shard(pub u32);
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Transaction {
     pub id: u32,
     pub shards: Vec<Shard>,
     pub effective_fee: u32,
 }
 
-impl PartialEq<Self> for Transaction {
+#[derive(Debug)]
+pub struct SortableByFeeTransaction {
+    pub tx: Arc<Transaction>,
+    pub fee: u32,
+}
+
+impl PartialEq<Self> for SortableByFeeTransaction {
     fn eq(&self, other: &Self) -> bool {
-        self.effective_fee == other.effective_fee
+        self.fee == other.fee && self.tx.id == other.tx.id
     }
 }
 
-impl PartialOrd for Transaction {
+impl PartialOrd for SortableByFeeTransaction {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.effective_fee.partial_cmp(&other.effective_fee)
+        self.fee.partial_cmp(&other.fee).and_then(|o| {
+            if o == Ordering::Equal {
+                self.tx.id.partial_cmp(&other.tx.id)
+            } else {
+                Some(o)
+            }
+        })
     }
 }
 
-impl Eq for Transaction {}
+impl Eq for SortableByFeeTransaction {}
 
-impl Ord for Transaction {
+impl Ord for SortableByFeeTransaction {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.effective_fee.cmp(&other.effective_fee)
+        self.fee.cmp(&other.fee).then(self.tx.id.cmp(&other.tx.id))
     }
 }
